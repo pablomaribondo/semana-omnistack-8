7f49edf6e8742b0d2989ef884ff6f678f221c58b
Pablo Maribondo <pablomgvieira@gmail.com>
2021-04-30 08:35:38 -0300

    (mobile) tela main implementada

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,186 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
-const Main = () => {
-  return <View />;
+import api from '../services/api';
+
+import logo from '../assets/logo.png';
+import dislike from '../assets/dislike.png';
+import like from '../assets/like.png';
+
+const Main = ({navigation}) => {
+  const id = navigation.getParam('id');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('devs', {
+        headers: {
+          user: id,
+        },
+      });
+
+      setUsers(response.data);
+    })();
+  }, [id]);
+
+  const handleLike = async () => {
+    const [{_id}, ...rest] = users;
+    await api.post(`/devs/${_id}/likes`, null, {headers: {user: id}});
+
+    setUsers(rest);
+  };
+
+  const handleDislike = async () => {
+    const [{_id}, ...rest] = users;
+    await api.post(`/devs/${_id}/dislikes`, null, {headers: {user: id}});
+
+    setUsers(rest);
+  };
+
+  const handleLogout = async () => {
+    await AsyncStorage.clear();
+
+    navigation.navigate('Login');
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={handleLogout}>
+        <Image style={styles.logo} source={logo} />
+      </TouchableOpacity>
+
+      <View style={styles.cardsContainer}>
+        {users.length === 0 ? (
+          <Text style={styles.empty}>Acabou:(</Text>
+        ) : (
+          users.map((user, index) => (
+            <View
+              key={user._id}
+              style={[styles.card, {zIndex: users.length - index}]}>
+              <Image
+                style={styles.avatar}
+                source={{
+                  uri: user.avatar,
+                }}
+              />
+              <View style={styles.footer}>
+                <Text style={styles.name}>{user.name}</Text>
+                <Text style={styles.bio} numberOfLines={3}>
+                  {user.bio}
+                </Text>
+              </View>
+            </View>
+          ))
+        )}
+      </View>
+
+      {users.length > 0 && (
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleDislike}>
+            <Image source={dislike} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleLike}>
+            <Image source={like} />
+          </TouchableOpacity>
+        </View>
+      )}
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  logo: {
+    marginTop: 30,
+  },
+
+  empty: {
+    alignSelf: 'center',
+    color: '#999',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  cardsContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
+  },
+
+  card: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    margin: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+
+  avatar: {
+    flex: 1,
+    height: 300,
+  },
+
+  footer: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  bio: {
+    fontSize: 14,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 18,
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginBottom: 30,
+  },
+
+  button: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 2,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
+});
 
 export default Main;

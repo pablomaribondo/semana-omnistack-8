13f6ba24164d494a8158e2a7389fccbc0b0b6787
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-01 11:51:35 -0300

    (server) match implementado

--- server/src/controllers/LikeController.js
+++ server/src/controllers/LikeController.js
@@ -13,7 +13,16 @@ module.exports = {
     }
 
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log("MATCH");
+      const loggedSocket = request.connectedUsers[user];
+      const targetSocket = request.connectedUsers[devId];
+
+      if (loggedSocket) {
+        request.io.to(loggedSocket).emit("match", targetDev);
+      }
+
+      if (targetSocket) {
+        request.io.to(targetSocket).emit("match", loggedDev);
+      }
     }
 
     loggedDev.likes.push(targetDev._id);
--- server/src/index.js
+++ server/src/index.js
@@ -6,17 +6,35 @@ require("dotenv").config();
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3333;
-const server = express();
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cookie: false,
+});
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
 
 const MONGO_ATLAS_URI = process.env.MONGO_ATLAS_URI;
 mongoose.connect(MONGO_ATLAS_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+app.use((request, _response, next) => {
+  request.io = io;
+  request.connectedUsers = connectedUsers;
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+  return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
